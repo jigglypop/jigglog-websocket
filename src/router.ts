e26a557a40d6c5f833b564ab7734500d506eeda9
jigglypop <ydh2244@gmail.com>
2023-01-29 21:33:27 +0900

    기본메일앱

--- src/router.ts
+++ src/router.ts
@@ -2,5 +2,6 @@ import express from 'express';
 import { mailController } from './controller';
 
 const mailRouter = express();
+
 mailRouter.use("/api/mail", mailController);
 export default mailRouter;
